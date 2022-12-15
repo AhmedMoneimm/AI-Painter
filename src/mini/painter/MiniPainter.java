0d05f70904a8fe8d377555e6901bd45a86ff5487
Ahmed Abdelmoneim <ahmedmoneimaly@gmail.com>
2022-12-15 16:16:33 +0200

    .

---
+++ src/mini/painter/MiniPainter.java
@@ -0,0 +1,16 @@
+package mini.painter;
+
+import frontend.Menu;
+
+
+public class MiniPainter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Menu();
+    }
+    
+}
